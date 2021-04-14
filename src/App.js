cadd56e37fe7553dfa607b326451964a4e4b3cbb
Teajey <21069848+Teajey@users.noreply.github.com>
2021-04-14 16:15:11 +1200

    Minify svg

--- src/App.js
+++ src/App.js
@@ -26,18 +26,9 @@ const markers = Array(256)
 
 export function defaultGoogleMapsMarkerUrl(color = "#ea4335") {
   return (
-    "data:image/svg+xml;charset=UTF-8," +
+    "data:image/svg+xml," +
     encodeURIComponent(
-      `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" viewBox="-27 -27 54 86">
-        <path d="
-          M 22.51666049 13
-          A 26 26 0 1 0 -22.51666049 13
-          C -16.780896127 21.191520442 -3 40 -3 55
-          A 3 3 0 0 0 3 55
-          C 3 40 16.780896127 21.191520442 22.51666049 13"
-            fill="${color}" stroke="white" stroke-width="2" />
-        <circle cx="0" cy="0" r="9" fill="#0007" />
-      </svg>`
+      `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-27 -27 54 86"><path fill="${color}" stroke="#fff" stroke-width="2" d="M22.5 13a26 26 0 10-45 0C-16.8 21.2-3 40-3 55a3 3 0 006 0c0-15 13.8-33.8 19.5-42"/><circle r="9" fill="#0007"/></svg>`
     )
   );
 }
